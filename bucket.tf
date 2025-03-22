d9c1bf82d2812a89dee43ec4bc1f9ee9ce87c072
nathan <nathan@yeetbox.net>
2025-03-22 12:52:58 -0400

    initial commit

---
+++ bucket.tf
@@ -0,0 +1,14 @@
+resource "digitalocean_spaces_bucket" "bucket" {
+  name   = var.name
+  region = var.region
+  force_destroy = var.force_destroy
+
+  lifecycle_rule {
+    id = "expiration"
+    enabled = true
+    abort_incomplete_multipart_upload_days = 3
+    expiration {
+        days = var.expiration
+    }
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,34 @@
+variable "do_token" {
+    type = string
+    default = ""
+}
+
+variable "name" {
+    type = string
+    default = ""
+}
+
+variable "region" {
+    type = string
+    default = ""
+}
+
+variable "is_public" {
+  type        = string
+  default = "private"
+
+  validation {
+    condition     = contains(["private", "public-read"], var.is_public)
+    error_message = "Options are either private or public-read"
+  } 
+}
+
+variable "force_destroy" {
+    type = bool
+    default = false
+}
+
+variable "expiration" {
+    type = int
+    default = 30
+}
