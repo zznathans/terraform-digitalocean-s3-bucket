1b3ac3179451c471f41576675bd7a2ad4cbbc01e
nathan <nathan@zswap.net>
2026-03-21 14:28:00 -0400

    fix(project): correct bucket resource reference from arn to urn

--- project.tf
+++ project.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_project_resources" "project_resources" {
   project = data.digitalocean_project.project.id
   resources = [
-    digitalocean_spaces_bucket.bucket.arn
+    digitalocean_spaces_bucket.bucket.urn
   ]
 }
