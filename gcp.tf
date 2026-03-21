3a144ed835d095bf9278d75aee20018ae8de79f4
nathan <nathan@zswap.net>
2026-03-21 14:48:07 -0400

    fix(gcp): correct access key reference in secret data

--- gcp.tf
+++ gcp.tf
@@ -19,7 +19,7 @@ resource "google_secret_manager_regional_secret_version" "secret" {
   # Secret data is a JSON object so ESO can extract individual fields via
   # a SecretStore remoteRef with property: access_key / secret_key
   secret_data = jsonencode({
-    access_key  = digitalocean_spaces_key.access_keys[each.key].access_key_id
+    access_key  = digitalocean_spaces_key.access_keys[each.key].access_key
     secret_key  = digitalocean_spaces_key.access_keys[each.key].secret_key
     bucket_name = digitalocean_spaces_bucket.bucket.name
     endpoint    = "https://${var.region}.digitaloceanspaces.com"
